a81ad961d63f88f301237733c440bc1c70440fed
Randy Murrish <randy@stad.us>
2025-10-31 22:03:51 -0700

    add launchd daemon

---
+++ uzzu-transform.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar /Users/mush/src/m3u8-fixer/target/m3u8-fixer-1.0-SNAPSHOT-jar-with-dependencies.jar https://uzzu.tv/app2/arelem@mac.com -o /Users/mush/volumes/channels/m3u8/uzzu.m3u8
